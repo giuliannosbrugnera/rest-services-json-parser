a327dede1a53b99416756f29022e7f044d3b8ea2
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-16 14:02:34 -0300

    Minor changes

--- RESTServicesJSONParserExample/Program.cs
+++ RESTServicesJSONParserExample/Program.cs
@@ -41,14 +41,13 @@ namespace RESTServicesJSONParserExample
                 using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
                     if (response.StatusCode != HttpStatusCode.OK)
-                        throw new Exception(String.Format(
-                        "Server error (HTTP {0}: {1}).",
-                        response.StatusCode,
-                        response.StatusDescription));
+                    {
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.StatusDescription));
+                    }
                     DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Response));
                     object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
-                    Response jsonResponse
-                    = objResponse as Response;
+                    Response jsonResponse = objResponse as Response;
+
                     return jsonResponse;
                 }
             }

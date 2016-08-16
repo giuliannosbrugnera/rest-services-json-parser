c817f89d12c8c9ec0d179318e94fe54287300aaa
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-16 13:12:07 -0300

    Create the basic program

--- RESTServicesJSONParserExample/Program.cs
+++ RESTServicesJSONParserExample/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization.Json;
 
 namespace RESTServicesJSONParserExample
+
 {
     class Program
     {
+        static string BingMapsKey = "Aml6nTS3odM79EzwdEZhUtO_RIka7pVBJdsQJidAnuprqQUQjKd3vsG768Hwf9XI";
         static void Main(string[] args)
         {
+            try
+            {
+                string locationsRequest = CreateRequest("New%20York");
+                Response locationsResponse = MakeRequest(locationsRequest);
+                ProcessResponse(locationsResponse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
+
         }
     }
 }

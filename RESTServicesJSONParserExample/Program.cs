4c7b46ec5fad9e2c22ed68e8891ac028bcd54708
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-16 13:30:36 -0300

    Process the request

--- RESTServicesJSONParserExample/Program.cs
+++ RESTServicesJSONParserExample/Program.cs
@@ -58,5 +58,54 @@ namespace RESTServicesJSONParserExample
                 return null;
             }
         }
+
+        static public void ProcessResponse(Response locationsResponse)
+        {
+            int locNum = locationsResponse.ResourceSets[0].Resources.Length;
+
+            //Get formatted addresses
+            //Get all locations in the response and then extract the formatted address for each location
+            Console.WriteLine("Show all formatted addresses");
+            for (int i = 0; i < locNum; i++)
+            {
+                Location location = (Location)locationsResponse.ResourceSets[0].Resources[i];
+                Console.WriteLine(location.Address.FormattedAddress);
+            }
+            Console.WriteLine();
+
+            //Get the Geocode Points for each Location
+            for (int i = 0; i < locNum; i++)
+            {
+                Location location = (Location)locationsResponse.ResourceSets[0].Resources[i];
+                Console.WriteLine("Geocode Points for " + location.Address.FormattedAddress);
+                int geocodePointNum = location.GeocodePoints.Length;
+                for (int j = 0; j < geocodePointNum; j++)
+                {
+                    Console.WriteLine("    Point: " + location.GeocodePoints[j].Coordinates[0].ToString() + "," +
+                                                location.GeocodePoints[j].Coordinates[1].ToString());
+                    double test = location.GeocodePoints[j].Coordinates[1];
+                    Console.Write("    Usage: ");
+                    for (int k = 0; k < location.GeocodePoints[j].UsageTypes.Length; k++)
+                    {
+                        Console.Write(location.GeocodePoints[j].UsageTypes[k].ToString() + " ");
+                    }
+                    Console.WriteLine("\n\n");
+                }
+            }
+            Console.WriteLine();
+
+            //Get all locations that have a MatchCode=Good and Confidence=High
+            Console.WriteLine("Locations that have a Confidence=High");
+            for (int i = 0; i < locNum; i++)
+            {
+                Location location = (Location)locationsResponse.ResourceSets[0].Resources[i];
+                if (location.Confidence == "High")
+                    Console.WriteLine(location.Address.FormattedAddress);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
     }
 }
